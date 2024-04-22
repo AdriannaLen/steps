a883cabc08ee72ddc4f5eede4b116741de9ae8fc
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-04-22 14:03:34 +0200

    added emoji to <Button />

--- src/App.js
+++ src/App.js
@@ -55,24 +55,26 @@ return <div>
       bgColor= "#7950f2"
       textColor= "#fff"
       onClick={handlePrevious}
-      text="Previous" />
+      text="Previous"
+      emoji="👈" />
 
       <Button
       bgColor= "#7950f2"
       textColor= "#fff"
       onClick={handleNext}
-      text="Next" />
+      text="Next"
+      emoji="👉" />
     </div>
 
   </div>
   )} </div>
 }
 
-function Button({textColor, bgColor, onClick, text}) {
+function Button({textColor, bgColor, onClick, text, emoji}) {
 
   return (
 
     <button style={{backgroundColor: bgColor, color: textColor }}
-    onClick= {onClick}>{text}</button>
+    onClick= {onClick}><span>{emoji}</span>{text}</button>
   )
 }
